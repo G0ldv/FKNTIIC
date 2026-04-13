089796d6ba67d8961c3c26ff0d83ff0972f7d73f
Daniel <daniagolub3321@gmail.com>
2026-04-13 15:24:49 +0300

    Налаштував проксі та смайлики

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import asyncio
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand
+from aiogram.client.session.aiohttp import AiohttpSession
 from handlers import router as start_router
 from handlers.admission import router as admission_router
 from handlers.admin import router as admin_router
@@ -11,6 +12,7 @@ from handlers.college import router as college_router
 load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
 
+session = AiohttpSession(proxy="http://proxy.server:3128")
 
 dp = Dispatcher()
 dp.include_router(start_router)
@@ -19,9 +21,10 @@ dp.include_router(admin_router)
 dp.include_router(college_router)
 
 async def main():
-    bot = Bot(token=TOKEN)
+    # bot = Bot(token=TOKEN)
+    bot = Bot(token=TOKEN, session=session)
     await bot.set_my_commands([
-        BotCommand(command="start", description="🚀 Запуск бота"),
+        BotCommand(command="start", description="Запуск бота 🚀"),
         BotCommand(command="restart", description="🔄 Перезапустити бота"),
     ])
     print("Бот запущен...")
